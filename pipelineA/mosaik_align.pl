1d8278b9689cd4d00b019565c41067ab77407f08
brettin <brettin@anl.gov>
2014-07-20 09:28:19 +1000

    added -bam option and cleaned up input params and documentation

--- pipelineA/mosaik_align.pl
+++ pipelineA/mosaik_align.pl
@@ -1,69 +1,75 @@
+#!/usr/bin/env perl
+
 use File::Basename;
+use File::Path qw(make_path remove_tree);
 use strict;
 use Getopt::Long; 
 use Pod::Usage;
 
 my $help = 0;
+my ($build_dir, @mkbs, $align_dir, $ref_genome);
 
-my $fastq_dir = 'fastq';
-my $build_dir = "/mnt/data/biosamples/SAMN01828242/build";
-my $align_dir = "/mnt/data/biosamples/SAMN01828242/align";
-my $REFDB = "/mnt/data/reference";
-my $REFGENOME = "Mycobacterium_tuberculosis_H37Rv";
-my $ANNPE = "/usr/local/bin/2.1.78.pe.ann";
-my $ANNSE = "/usr/local/bin/2.1.78.se.ann";
-my $THREADS = 4;
+my $annpe = "/usr/local/bin/2.1.78.pe.ann";
+my $annse = "/usr/local/bin/2.1.78.se.ann";
+my $threads = 4;
 
-
+# required build_dir, align_dir, ref_genome
 GetOptions(
         'h'     => \$help,
 	'bd=s'  => \$build_dir,
 	'ad=s'  => \$align_dir,
-	'ref'   => \$REFGENOME,
-	'annpe=s' => \$ANNPE,
-	'annse=s' => \$ANNSE,
-	't=i'     => \$THREADS,
-	'refdb=s' => \$REFDB,
+	'rg=s'  => \$ref_genome,
+	'mkb=s' => \@mkbs,
+	'annpe=s' => \$annpe,
+	'annse=s' => \$annse,
+	't=i'     => \$threads,
 ) or pod2usage(0);
 
 pod2usage(-exitstatus => 0,
           -output => \*STDOUT,
           -verbose => 1,
           -noperldoc => 1,
-         ) if $help;
+         ) if $help or (!$align_dir)
+		    or (!$ref_genome)
+	            or ( (! $build_dir ) and ( ! @mkbs ) );
 
+die "ref_genome $ref_genome does not exist" unless -e $ref_genome;
 
-my (@end1, @end2, );
-foreach my $fastq (`ls $fastq_dir/*.fastq`) {
-  push @end1, $fastq if $fastq =~ /_1.fastq/;
-  push @end2, $fastq if $fastq =~ /_2.fastq/;
-}
-map chomp, @end1;
-map chomp, @end2;
-
-# do a bit of error checking
-if (@end1 != @end2) {
-  die "could not find matching pe files in fastq_dir: $fastq_dir";
+if ( ! -d $align_dir ) {
+  make_path($align_dir, { verbose => 1 });
 }
 
-print "processing files ", join ", ", @end1, @end2, "\n";
+
+if ( @mkbs ) {
+  @mkbs = split( /,/, join( ',', @mkbs ) );
+}
+elsif ( -d $build_dir ) {
+  foreach my $mkb ( glob( "$build_dir/*.mkb" ) ) {
+    push @mkbs, $mkb;
+  }
+}
+else {
+  die "Houston, we have a problem (no mkbs and no build_dir)";
+}
+
+
+print "processing files ", join ", ", @mkbs, "\n";
+
 
 # this should be parallel
-for (my $i=0; $i<@end1; $i++) {
+my @cmds = ();
 
-  # get the basename of each pe set
-  my $name1 = fileparse($end1[$i], ("_1.fastq", "_1.fastq.gz"));
-  my $name2 = fileparse($end2[$i], ("_2.fastq", "_2.fastq.gz"));
-  if ($name1 ne $name2) {
-    print "files don't match $end1[$i] $end2[$i]\n";
-    next;
-  }
+for (my $i=0; $i<@mkbs; $i++) {
+  my ($name, $path) = fileparse $mkbs[$i];
 
-  my $cmd1 = "MosaikAligner -in build/$name1.mkb -out align/$name1.mka ";
-  $cmd1.= "-ia $REFDB/$REFGENOME.dat -annpe $ANNPE -annse $ANNSE ";
-  $cmd1.="-p $THREADS";
+  my $cmd1 = "MosaikAligner -in $mkbs[$i] ";
+  $cmd1   .= "-out $align_dir/$name ";
+  $cmd1   .= "-ia $ref_genome -annpe $annpe -annse $annse ";
+  $cmd1   .="-p $threads";
 
   print $cmd1, "\n";
+
+  push @cmds, $cmd1;
 
   unless (!system $cmd1 ) {
     print "failed running $cmd1\n$!"; 
@@ -83,21 +89,12 @@ mosaik_aling.pl
 
 =head1  SYNOPSIS
 
-Arguements and defaults
-
-
- my $fastq_dir = 'fastq';
- my $build_dir = "/mnt/data/biosamples/SAMN01828242/build";
- my $align_dir = "/mnt/data/biosamples/SAMN01828242/align";
- my $REFDB = "/mnt/data/reference";
- my $REFGENOME = "Mycobacterium_tuberculosis_H37Rv";
- my $ANNPE = "/usr/local/bin/2.1.78.pe.ann";
- my $ANNSE = "/usr/local/bin/2.1.78.se.ann";
- my $THREADS = 4;
+ mosaik_align.pl -bd <build_dir> -ad <align_dir> -rg <reference_genome>
+ mosaik_align.pl -mkb <comma separeated list of mkb files> -rg <reference_genome>
 
 =head1  DESCRIPTION
 
-Wrapper for the MosaikAlign program.
+Wrapper for the MosaikAlign program. Takes as input a reference genome that has been fomatted using mosaik_build (or MosaikBuild directly) and either a directory containing the set of mosaik build files or a comma separated (no whitespaces) list of mosaik build files.
 
 =head1  OPTIONS
 
@@ -105,40 +102,44 @@ Wrapper for the MosaikAlign program.
 
 =item   -h
 
-Basic usage documentation
+ Basic usage documentation
 
 =item	-bd
 
-The location of the output of the MosaikBuild command (build_dir).
+ The location of the output of the MosaikBuild command (build_dir).
+ Either this option, or the -mkb option is required. Both can not be used.
+
+=item	-mkb
+
+ A comma separated (no whitespaces) list of the MosaikBuild files.
+ Either this option, or the -bd option is required. Bot can not be used.
 
 =item	-ad
 
-The location of the directory to put the bam files (align_dir).
+ The location of the directory to put the bam files (align_dir).
 
-=item	-annpe=s
+=item	-rg
 
-ANNPE,
+ The reference genome to align to. This should include the path. This
+ is the output file created by mosaik_build_ref.pl
 
-=item	-annse=s
+=item	-annpe
 
-ANNSE,
+ The neural net training file for paired end reads. The default is
+ /usr/local/bin/2.1.78.pe.ann
+
+=item	-annse
+
+ The neural net training file for single end reads. The default is
+ /usr/local/bin/2.1.78.se.ann
 
 =item	-t
 
-Number of threads that the aligner should use.
-
-=item	-ref
-
-The filename of the reference genome (prefix only).
-
-=item	-refdb
-
-Path to the directory containing the reference genome.
+ Number of threads that the aligner should use. The default is 1.
 
 =back
 
 =head1  AUTHORS
 
-[% kb_author %]
 
 =cut
