3ac6af6d78579d17918838f3f9c20ceceea7e0ae
brettin <brettin@anl.gov>
2014-08-31 05:36:23 +0000

    added option fo specify output file names

--- scripts/va-awe_mosaik_build.pl
+++ scripts/va-awe_mosaik_build.pl
@@ -10,7 +10,7 @@ use strict;
 my $man  = 0;
 my $help = 0;
 my ($fastq_dir, $build_dir, $file_suffix_1, $file_suffix_2);
-my (@end1, @end2, );
+my (@end1, @end2, @outfiles, );
 
 # the suffix on the mate pair files used to construct basename
 $file_suffix_1 = '_1.fastq';
@@ -24,22 +24,27 @@ GetOptions(
 	'fs2=s' => \$file_suffix_2,
 	'fq1=s'  => \@end1,
 	'fq2=s'  => \@end2,
+	'o=s'   => \@outfiles,
 ) or pod2usage(0);
 
 pod2usage(-exitstatus => 0,
           -output => \*STDOUT,
           -verbose => 1,
           -noperldoc => 1,
-         ) if $help or ( ! $build_dir )
+         ) if $help or ( (! $build_dir) and (! @outfiles))
                     or ( (! $fastq_dir) and ( ( ! @end1 ) or ( ! @end2 ) ) ) ;
 
 
 if( @end1 and @end2 ) {
   @end1 = split(/,/,join(',',@end1));
   @end2 = split(/,/,join(',',@end2));
+    if ( @outfiles ) {
+      @outfiles = split(/,/,join(',',@outfiles));
+    }
 }
+
 elsif ($fastq_dir) {
-  foreach my $fastq (`ls $fastq_dir/*.fastq`) {
+  foreach my $fastq (`ls $fastq_dir/*.fastq`, `ls $fastq_dir/*.fastq.gz`) {
     push @end1, $fastq if $fastq =~ /$file_suffix_1/;
     push @end2, $fastq if $fastq =~ /$file_suffix_2/;
   }
@@ -54,7 +59,7 @@ if (@end1 != @end2) {
   die "could not find matching pe files in fastq_dir: $fastq_dir";
 }
 
-if (! -d $build_dir ) {
+if ($build_dir and (! -d $build_dir )) {
   make_path($build_dir, { verbose => 1 });
 }
 
@@ -74,7 +79,8 @@ for (my $i=0; $i<@end1; $i++) {
 
   # -mfl is median fragment length. 
   my $cmd1 = "MosaikBuild -q $end1[$i] -q2 $end2[$i] -st illumina -mfl 500 ";
-  $cmd1.= "-out $build_dir/$name1.mkb";
+  $cmd1.= "-out $build_dir/$name1.mkb" if $build_dir;
+  $cmd1.= "-out $outfiles[$i]" if @outfiles;
   push @cmds, $cmd1;
 }
 
@@ -167,6 +173,14 @@ A list of fastq files representing 1 end of a mate pair.
 
 A list of fastq files representing the other end of the mate pair.
 The order of the files in -fq2 muust pair with the order in -fq1.
+Either the -o or the -bd options can be used for the output files.
+
+=item	-o
+
+A list of output file names. This only works if -fq1 and -fq2 are set.
+The order of the files in -o will map to the order of the files in
+-fq1 and -fq2. An error is thrown if the number of output files does
+not equal the number of input pairs.
 
 =back
 
