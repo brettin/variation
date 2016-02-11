7f8de9c64e2946018e8aed5c49bf9dd45a582e71
brettin <brettin@anl.gov>
2016-02-11 15:17:32 +0000

    use shock handle for reference genome database

--- scripts/va-mosaik_align.pl
+++ scripts/va-mosaik_align.pl
@@ -2,25 +2,24 @@
 
 use File::Basename;
 use File::Path qw(make_path remove_tree);
+use MIME::Base64;
+use JSON;
 use strict;
 use Getopt::Long; 
 use Pod::Usage;
 
 my $help = 0;
-my ($build_dir, @mkbs, $align_dir, @outfiles, $ref_genome);
+my ($build_dir, @mkbs, $align_dir, @outfiles, $ref_db);
 
 my $annpe = "/usr/local/bin/2.1.78.pe.ann";
 my $annse = "/usr/local/bin/2.1.78.se.ann";
 my $threads = 4;
 
-# required build_dir, align_dir, ref_genome
 GetOptions(
-        'h'     => \$help,
-	'bd=s'  => \$build_dir,
+  'h'     => \$help,
 	'ad=s'  => \$align_dir,
-	'rg=s'  => \$ref_genome,
+	'rdh=s'  => \$ref_db,
 	'mkb=s' => \@mkbs,
-	'o=s'   => \@outfiles,
 	'annpe=s' => \$annpe,
 	'annse=s' => \$annse,
 	't=i'     => \$threads,
@@ -30,50 +29,54 @@ pod2usage(-exitstatus => 0,
           -output => \*STDOUT,
           -verbose => 1,
           -noperldoc => 1,
-         ) if $help or ((!$align_dir) and (!@outfiles))
-		    or (!$ref_genome)
-	            or ( (! $build_dir ) and ( ! @mkbs ) );
+         ) if $help 
+		    or ( !$ref_db )
+        or ( !@mkbs );
 
-die "ref_genome $ref_genome does not exist" unless -e $ref_genome;
+@mkbs = split( /,/, join( ',', @mkbs ) );
 
+# decode the reference db handle
+my $json_handle = decode_base64($ref_db);
+my $perl_handle = decode_json($json_handle);
+my $tarball = $perl_handle->{file_name};
+my $ref_db_dir = $1 if $tarball =~ /(\S+)\.tar/;
+die "could not parse ref_db_dir from $tarball" unless $ref_db_dir;
+
+# fetch the reference db tarball from shock
+open  H, ">$tarball.handle" or die "can not write handle to disk";
+print H $json_handle;
+close H;
+
+!system("kbhs-download", "-handle", "$tarball.handle", "-o", "$perl_handle->{file_name}")
+  or die "failed to execute kbhs-download", $?;
+!system("tar", "-xvf", $perl_handle->{file_name})
+  or die "can not untar $perl_handle->{file_name}", $?;
+my $ref_genome = "$ref_db_dir/$ref_db_dir.mkb";
+-e $ref_genome
+  or die "could not find reference genome named $ref_genome";
+print "ref_genome: $ref_genome\n";
+
+# set up the alignments output directory
 if ( $align_dir and (! -d $align_dir )) {
   make_path($align_dir, { verbose => 1 });
 }
-
-
-if ( @mkbs ) {
-  @mkbs = split( /,/, join( ',', @mkbs ) );
-  if ( @outfiles ) {
-    @outfiles = split( /,/, join( ',', @outfiles ) );
-  }
-}
-elsif ( -d $build_dir ) {
-  foreach my $mkb ( glob( "$build_dir/*.mkb" ) ) {
-    push @mkbs, $mkb;
-  }
-}
 else {
-  die "Houston, we have a problem (no mkbs and no build_dir)";
+  $align_dir = '.';
 }
-
 
 print "processing files ", join ", ", @mkbs, "\n";
 
-
-# this should be parallel
+# this could be parallelized
 my @cmds = ();
 
 for (my $i=0; $i<@mkbs; $i++) {
-  my ($name, $path) = fileparse $mkbs[$i];
-
+  my ($name, $path, $suffix) = fileparse $mkbs[$i], '.mkb';
   my $cmd1 = "MosaikAligner -in $mkbs[$i] ";
-  $cmd1   .= "-out $align_dir/$name " if $align_dir;
-  $cmd1   .= "-out $outfiles[$i] " if @outfiles and ! $align_dir;
+  $cmd1   .= "-out $align_dir/$name ";
   $cmd1   .= "-ia $ref_genome -annpe $annpe -annse $annse ";
   $cmd1   .="-p $threads";
 
   print $cmd1, "\n";
-
   push @cmds, $cmd1;
 
   unless (!system $cmd1 ) {
@@ -94,12 +97,17 @@ mosaik_aling.pl
 
 =head1  SYNOPSIS
 
- mosaik_align.pl -bd <build_dir> -ad <align_dir> -rg <reference_genome>
- mosaik_align.pl -mkb <comma separeated list of mkb files> -rg <reference_genome>
+ mosaik_align.pl -mkb <comma separeated list of mkb files> -rdh <handle file name>
 
 =head1  DESCRIPTION
 
 Wrapper for the MosaikAlign program. Takes as input a reference genome that has been fomatted using mosaik_build (or MosaikBuild directly) and either a directory containing the set of mosaik build files or a comma separated (no whitespaces) list of mosaik build files.
+
+  'rg=s'  => \$ref_db,
+  'mkb=s' => \@mkbs,
+  'annpe=s' => \$annpe,
+  'annse=s' => \$annse,
+  't=i'     => \$threads,
 
 =head1  OPTIONS
 
@@ -109,11 +117,6 @@ Wrapper for the MosaikAlign program. Takes as input a reference genome that has 
 
  Basic usage documentation
 
-=item	-bd
-
- The location of the output of the MosaikBuild command (build_dir).
- Either this option, or the -mkb option is required. Both can not be used.
-
 =item	-mkb
 
  A comma separated (no whitespaces) list of the MosaikBuild files.
@@ -122,11 +125,20 @@ Wrapper for the MosaikAlign program. Takes as input a reference genome that has 
 =item	-ad
 
  The location of the directory to put the bam files (align_dir).
+ Default location is the current working directory.
 
-=item	-rg
+=item	-rdh
 
- The reference genome to align to. This should include the path. This
- is the output file created by mosaik_build_ref.pl
+ The reference genome database handle. This should be file that
+contains a shock handle that resolves to a tarball that contains the
+reference genome that will be used by the read mapper. See below for
+further details.
+
+   1. The reference genome is contained in a directory
+   2. The directory is named as the basename of the reference genome
+      file
+   3. The reference genome file suffix is .mkb
+   4. The handle resolves to a tarball of the above directory
 
 =item	-annpe
 
