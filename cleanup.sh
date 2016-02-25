4dbb9e4a0432127aecffe5cb6f70e13ae23ec0fd
brettin <brettin@anl.gov>
2016-02-25 17:57:13 -0600

    helper to remove build files

---
+++ cleanup.sh
@@ -0,0 +1,11 @@
+rm lib/VariationImpl.py*
+rm lib VariationServer.py
+rm lib/Variation.psgi
+rm lib/Bio/KBase/Variation/VariationImpl.pm.bak*
+rm lib/Bio/KBase/Variation/Client.pm
+rm lib/Bio/KBase/Variation/Service.pm
+rm lib/VariationServer.py
+rm -r lib/biokbase/
+rm -r lib/javascript/
+rm docs/Variation.html
+rm pod2htmd.tmp
