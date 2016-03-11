c4e83fb970baa3b8d2aeed8b3ebd9e5d743b3e9f
brettin <brettin@anl.gov>
2016-03-11 08:59:55 -0600

    switching from gid to genome_name

--- lib/Bio/KBase/Variation/libdb.pm
+++ lib/Bio/KBase/Variation/libdb.pm
@@ -1,9 +1,12 @@
 package Bio::KBase::Variation::libdb;
+use Bio::KBase::Variation::VariationConstants qw(p3dataurl);
 
 use IPC::Run qw(run timeout);
 use JSON;
 use Template;
+use Cwd;
 use File::Basename;
+use File::Temp qw(tempdir);
 use Exporter qw(import);
 
 our @EXPORT = qw(variation_build_db);
@@ -28,33 +31,28 @@ our %EXPORT_TAGS = (
   ]
 );
 
+our $data_url = p3dataurl;
+our $samtools_exe = "$ENV{KB_RUNTIME}/samtools/bin/samtools";
+-e $samtools_exe or die "samtools_exe does not exist, check KB_RUNTIME";
 
-# patric variables
-our $data_url = "https://www.patricbrc.org/api";
-# our $gid = "272559.17";
-# our $genome_name = "Bacteroides_fragilis_NCTC_9343";
-
-# variation variables (this is where snpeff expects to find $ver)
-# our $data_dir = "/tmp/reference";
-
-# snpeff variables
-# our $snpeff_config = "/tmp/snpEff/snpEff.config";
-# our $ver = 'p3.' . $gid;
-# our $snpeff_config_tt = '/home/ubuntu/tom/dev_container/modules/variation/scripts/snpEff.config.tt';
 my ($name,$path,$suffix) = fileparse( $INC{"Bio/KBase/Variation/libdb.pm"} );
-our $snpeff_config_tt =  $path . "/snpEff.config.tt";;
+our $snpeff_config_tt =  $path . "snpEff.config.tt";;
+our $dbpath = "/tmp/reference";
+our $dbpath = tempdir( "snpeff-XXXXXX", DIR => "/tmp");
 
+print "using $data_url as the data url\n";
+print "using $dbpath as the refdb base dir\n";
+print "using $snpeff_config_tt as the snpeff.config template\n";
 
 sub variation_build_db {
   my $gid = shift or die "must provide genome id";
 
-  # create database directory
+  # create database directory in $dbpath
   my $json_text = get_json_genome($gid);
   my $perl_scalar = decode_json($json_text);
-  my $genome = $perl_scalar->{genome_name};
-  $genome =~ s/\s/_/g;
-  my $base = "/tmp/reference";
-  my $data_dir = $base . "/$genome";
+  my $genome_name = $perl_scalar->{genome_name};
+  $genome_name =~ s/\s/_/g;
+  my $data_dir = $dbpath . "/$genome_name";
   !system("mkdir", "-p", $data_dir)
     or die "can not mkdir $data_dir";
 
@@ -62,39 +60,70 @@ sub variation_build_db {
   my $fasta = get_fasta($gid);
   my $gff = get_gff($gid);
 
-  # create snpeff directory
-  snpeff_write_config( $gid, $data_dir );
-  snpeff_build_reference( $gid, $fasta, $gff, $data_dir );
+  #  snpeff directory
+  # snpeff_write_config( $gid, $data_dir );
+  # snpeff_build_reference( $gid, $fasta, $gff, $data_dir );
+
+  snpeff_write_config( $genome_name, $data_dir );
+  snpeff_build_reference( $genome_name, $fasta, $gff, $data_dir );
 
   # write genome.fna
-  write_to_file($fasta, "$data_dir/$genome.fna");
+  write_to_file($fasta, "$data_dir/$genome_name.fna");
   
   # write genome.mkb
-  my @cmd = ("MosaikBuild", "-fr", "$data_dir/$genome.fna", "-oa", "$data_dir/$genome.mkb");
+  my @cmd = ("MosaikBuild", "-fr", "$data_dir/$genome_name.fna", "-oa", "$data_dir/$genome_name.mkb");
   my ($out, $err) = run_cmd(\@cmd);
 
   # write genome.fai
-  build_fasta_index("$data_dir/$genome.fna");
-  # tar
+  build_fasta_index("$data_dir/$genome_name.fna");
+
   # upload to shock
+  my $handle = upload_dir_to_shock($data_dir);
+  return $handle;
 }
+
+
+
+
+# creates a tarball and uploads to shock returning
+# the handle file name.
+sub upload_dir_to_shock {
+  my $data_dir = shift or die "must provide data dir";
+  -d $data_dir or die "data_dir is not a directory";
+  my $oridir = cwd();
+  my $dirname  = dirname($data_dir);
+  my $basename = basename($data_dir);
+
+  chdir $dirname or die "can not chdir to $dirname";
+  my @cmd = ("tar", "-cvf", "$basename.tar", $basename);
+  my ($out, $err) = run_cmd(\@cmd);
+
+  @cmd = ("kbhs-upload", "-i", "$basename.tar", "-o", "$basename.tar.handle");
+  my ($out, $err) = run_cmd(\@cmd);
+
+  chdir $oridir or die "can not chdir to $oridir";
+  return $dirname . "/" . $basename . ".tar.handle";
+}
+
+
+# $data_dir is used as the data.dir property in snpeff.config
+# $gid is patric genome id
 
 sub snpeff_write_config {
   my $gid = shift or die "must provide genome id";
   my $data_dir = shift or die "must provide data dir";
   -d $data_dir or die "data dir is not a valid directory";
 
-  my $ver = 'p3.' . $gid;
   my $json_text = get_json_genome($gid);
   my $perl_scalar = decode_json($json_text);
   my $genome_name = $perl_scalar->{genome_name};
   $genome_name =~ s/\s/_/g;
 
-  my %data = ( 'data_dir'     => $data_dir,
-               'lof_ignoreProteinCodingAfter' => "0.95",
-               'lof_ignoreProteinCodingBefore' => "0.05",
-               'genome_entry'   => "$ver.genome : $perl_scalar->{genome_name}",
-             );
+  my %data = ( 'data_dir'     => '.', 
+	'lof_ignoreProteinCodingAfter' => "0.95",
+	'lof_ignoreProteinCodingBefore' => "0.05",
+	'genome_entry'   => "$gid.genome : $perl_scalar->{genome_name}",
+	);
   my $tt = Template->new({ABSOLUTE => 1});
   $tt->process($snpeff_config_tt, \%data, "$data_dir/snpEff.config") or die $tt->error;
 }
@@ -103,7 +132,7 @@ sub build_fasta_index {
   my $file_name = shift or die "must provide a file name";
   -e $file_name or die "$filename doesn't exist";
 
-  my @cmd = ("samtools", "faidx", $file_name);
+  my @cmd = ("$samtools_exe", "faidx", $file_name);
   my ($out, $err) = run_cmd(\@cmd);
   -e "$file_name.fai" or die "could not find fasta index";
 }
@@ -115,7 +144,7 @@ sub snpeff_build_reference {
   my $data_dir = shift or die "must provide a data dir";
   -d $data_dir or die "data dir is not a valid directory";
 
-  my $ver = "p3." . $gid;
+  my $ver = $gid;
 
   print "making dir $data_dir/$ver" , "\n";
   !system ( "mkdir", "-p", "$data_dir/$ver" )
@@ -174,13 +203,5 @@ sub run_cmd {
     or die "Error running cmd=@$cmd, stdout:\n$out\nstderr:\n$err\n";
   return ($out, $err);
 }
-
-sub unpackage_refdb {}
-
-sub reference_genome_fasta {}
-
-sub reference_genome_snpeff {}
-
-sub reference_genome_mkb {}
 
 1;
